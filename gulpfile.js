20c1296d2b4d544e270db10cd45d32abfef38a3f
Cristina Perea <criperrui@gmail.com>
2017-04-09 20:55:10 +0200

    Task iniciales

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+
+// Styles
+
+gulp.task('styles', function() {
+    console.log("Task de estilos");
+});
+
+// Scripts
+
+gulp.task('scripts', function() {
+    console.log("Task de scripts");
+});
+
+// Images
+
+gulp.task('images', function() {
+    console.log("Task de imagenes");
+});
+
+
+// Default
+
+gulp.task('default', function() {
+    console.log("Task default");
+});
